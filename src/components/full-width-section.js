c3ba2c6fb9d5f8b4a74c758d5497b48c1475a307
jaking-Z <381158345@qq.com>
2015-12-18 00:23:57 +0800

    css

--- src/components/full-width-section.js
+++ src/components/full-width-section.js
@@ -2,14 +2,45 @@
 
 var React = require('react');
 var ClearFix = require('material-ui/lib/clearfix');
+var Styles = require('material-ui/lib/styles');
+var DesktopGutter = Styles.Spacing.desktopGutter;
+var StylePropable = require('material-ui/lib/mixins').StylePropable;
 
 var FullWidthSection = React.createClass({
+	mixins: [StylePropable],
 	render: function(){
+		var style = this.props.style;
+	    var styles = this.getStyles();
+
 		return (
-			<ClearFix>
+			<ClearFix style={this.mergeAndPrefix(
+	          styles.root,
+	          style,
+	          styles.rootWhenLarge)}>
 				{this.props.children}
 			</ClearFix>
 			);
+	},
+
+	getStyles: function() {
+		return {
+		  root: {
+		    padding: DesktopGutter + 'px',
+		    boxSizing: 'border-box',
+		  },
+		  content: {
+		    maxWidth: '1200px',
+		    margin: '0 auto',
+		  },
+		  rootWhenSmall: {
+		    paddingTop: (DesktopGutter * 2) + 'px',
+		    paddingBottom: (DesktopGutter * 2) + 'px',
+		  },
+		  rootWhenLarge: {
+		    paddingTop: (DesktopGutter * 3) + 'px',
+		    paddingBottom: (DesktopGutter * 3) + 'px',
+		  },
+		};
 	}
 });
 
--- src/components/master.js
+++ src/components/master.js
@@ -7,54 +7,132 @@ var Paper = require('material-ui/lib/paper');
 var Tabs = require('material-ui/lib/tabs/tabs');
 var Tab = require('material-ui/lib/tabs/tab');
 var FullWidthSection = require('./full-width-section');
+var Styles = require('material-ui/lib/styles');
+var Spacing = Styles.Spacing;
+var Colors = Styles.Colors;
+var Typography = Styles.Typography;
+var ThemeManager = Styles.ThemeManager;
+var DefaultRawTheme = Styles.LightRawTheme;
+var StylePropable = require('material-ui/lib/mixins').StylePropable;
 console.log(AppCanvas);
 var Master = React.createClass({
-	getInitialState: function(){
-		return {
-			tabIndex: 1
-		};
-	},
-	render: function(){
-		return (
-			<AppCanvas>
-				{this._getTabs()}
-				{this.props.children}
-				<FullWidthSection>
-					<p>联系我们</p>
-				</FullWidthSection>
-			</AppCanvas>
-			);
-	},
-	_getTabs: function(){
-		return '';
-		var productIcon = (
-				<EnhancedButton linkButton={true} href="/#/home">
-					<img src="" />
-					<span>产品名称</span>
-				</EnhancedButton>
-			);
-		return (
-			<div>
-				<Paper zDepth={0} rounded={false}>
-					{productIcon}
-					<div>
-						<Tabs 
-							value={this.state.tabIndex} 
-							onChange={this._handleTabChange}>
-							<Tab 
-								value="1" 
-								lable="Home" 
-								route="" />
-							<Tab 
-								value="1" 
-								lable="Home" 
-								route="" />
-						</Tabs>
-					</div>
-				</Paper>
-			</div>
-			);
-	}
+	mixins: [StylePropable],
+    getInitialState: function() {
+    	var muiTheme = ThemeManager.getMuiTheme(DefaultRawTheme);
+        return {
+            tabIndex: '1',
+            muiTheme: muiTheme
+        };
+    },
+    render: function() {
+        var styles = this.getStyles();
+        return ( <AppCanvas> 
+		        	{this._getTabs()} 
+		        	{this.props.children} 
+		        	<FullWidthSection style={styles.footer}>
+			            <p style={this.prepareStyles(styles.p)}> 联系我们 </p> 
+		            </FullWidthSection> 
+	            </AppCanvas>
+        );
+    },
+    _getTabs: function() {
+        var styles = {
+            root: {
+                backgroundColor: Colors.cyan500,
+                position: 'fixed',
+                height: 64,
+                top: 0,
+                right: 0,
+                zIndex: 1101,
+                width: '100%',
+            },
+            container: {
+                position: 'absolute',
+                right: (Spacing.desktopGutter / 2) + 48,
+                bottom: 0,
+            },
+            span: {
+                color: Colors.white,
+                fontWeight: Typography.fontWeightLight,
+                left: 45,
+                top: 22,
+                position: 'absolute',
+                fontSize: 26,
+            },
+            svgLogoContainer: {
+                position: 'fixed',
+                width: 300,
+                left: Spacing.desktopGutter,
+            },
+            svgLogo: {
+                width: 65,
+                backgroundColor: Colors.cyan500,
+                position: 'absolute',
+                top: 20,
+            },
+            tabs: {
+                width: 425,
+                bottom: 0,
+            },
+            inkBarStyle: {
+                backgroundColor: '#fff59d',
+            },
+            tab: {
+                height: 64,
+            }
+
+        };
+        var productIcon = ( 
+        	<EnhancedButton style={styles.svgLogoContainer} linkButton={true} href="/#/home">
+	            <img style={this.prepareStyles(styles.svgLogo)} src="" />
+	            <span style={this.prepareStyles(styles.span)}> 产品名称 </span> 
+            </EnhancedButton>
+        );
+        return ( 
+        	<div>
+	            <Paper style={styles.root} zDepth={0} rounded={false}> {productIcon} 
+		            <div style={this.prepareStyles(styles.container)}>
+			            <Tabs style={styles.tabs} value={this.state.tabIndex}
+                         inkBarStyle={styles.inkBarStyle}
+			             onChange={this._handleTabChange}>
+				            <Tab style={styles.tab} value="1"
+					             label="Home"
+					             route="/home"/>
+                            <Tab style={styles.tab} value="2"
+                                 label="Home2"
+                                 route=""/>
+                            <Tab style={styles.tab} value="2"
+                                 label="Home2"
+                                 route=""/>
+                            <Tab style={styles.tab} value="2"
+                                 label="Home2"
+                                 route=""/>
+			            </Tabs> 
+		            </div> 
+	            </Paper> 
+            </div>
+        );
+    },
+    getStyles: function() {
+    var darkWhite = Colors.darkWhite;
+    return {
+      footer: {
+        backgroundColor: Colors.grey900,
+        textAlign: 'center',
+      },
+      p: {
+        margin: '0 auto',
+        padding: 0,
+        color: Colors.lightWhite,
+        maxWidth: 335,
+      }
+    };
+  },
+
+  _handleTabChange: function(value, e, tab) {
+    // this.props.history.pushState(null, tab.props.route);
+    // this.setState({tabIndex: this._getSelectedIndex()});
+  }
 });
 
 module.exports = Master;
